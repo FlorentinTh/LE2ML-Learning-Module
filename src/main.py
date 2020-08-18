b1e84fb8a4a3f842c11deb36a48324d10211e694
FlorentinTh <florentin.thullier1@uqac.ca>
2020-08-18 18:02:53 -0400

    Continue test code.

--- src/main.py
+++ src/main.py
@@ -4,6 +4,12 @@ import requests
 import urllib3
 import json
 import pandas
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import cross_val_score, train_test_split, KFold
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn import metrics
 
 # Read a constant from .env file
 dotenv.load_dotenv()
@@ -27,4 +33,38 @@ print(algo_name)
 data_file_path = os.path.join(
     os.path.abspath(os.path.curdir),
     'job', 'data.csv')
-data = pandas.read_csv(data_file_path)
+data = pandas.read_csv(data_file_path, skiprows=1, header=None)
+num_col = len(data.columns)
+
+data_numeric = data.apply(pandas.to_numeric, errors='coerce')
+X = data_numeric.loc[:, :num_col - 2].values
+
+y = data.loc[:, num_col - 1].values
+
+le = LabelEncoder()
+y = le.fit_transform(y)
+
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, train_size=0.66, random_state=1)
+
+stdsc = StandardScaler()
+# X_train_std = stdsc.fit_transform(X_train)
+# X_test_std = stdsc.transform(X_test)
+
+X_std = stdsc.fit_transform(X)
+
+classifier = KNeighborsClassifier(
+    n_neighbors=1, metric='manhattan', algorithm='brute')
+
+# classifier = RandomForestClassifier(
+#     n_estimators=300, criterion='entropy', max_features='log2', n_jobs=-1)
+
+# classifier.fit(X_train_std, y_train)
+# model = classifier.predict(X_test_std)
+
+# accuracy = metrics.accuracy_score(y_test, model)
+# kappa = metrics.cohen_kappa_score(y_test, model)
+
+kfold = KFold(n_splits=10, random_state=100, shuffle=True)
+results_kfold = cross_val_score(classifier, X_std, y, cv=kfold)
+print("Accuracy: %.2f%%" % (results_kfold.mean()*100.0))
