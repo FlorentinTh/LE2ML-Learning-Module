32f0bb55605a38991f8f7c74fb6cfb27e5434cd5
FlorentinTh <florentin.thullier1@uqac.ca>
2020-08-19 17:56:22 -0400

    Finish all test code. Need clean up.

--- src/main.py
+++ src/main.py
@@ -3,13 +3,14 @@ import dotenv
 import requests
 import urllib3
 import json
-import pandas
+import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.model_selection import cross_val_score, train_test_split, KFold
+from sklearn.model_selection import cross_val_score, cross_val_predict, train_test_split, KFold
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 from sklearn import metrics
+import pickle
 
 # Read a constant from .env file
 dotenv.load_dotenv()
@@ -33,16 +34,16 @@ print(algo_name)
 data_file_path = os.path.join(
     os.path.abspath(os.path.curdir),
     'job', 'data.csv')
-data = pandas.read_csv(data_file_path, skiprows=1, header=None)
+data = pd.read_csv(data_file_path, skiprows=1, header=None)
 num_col = len(data.columns)
 
-data_numeric = data.apply(pandas.to_numeric, errors='coerce')
+data_numeric = data.apply(pd.to_numeric, errors='coerce')
 X = data_numeric.loc[:, :num_col - 2].values
 
 y = data.loc[:, num_col - 1].values
 
-le = LabelEncoder()
-y = le.fit_transform(y)
+# le = LabelEncoder()
+# y = le.fit_transform(y)
 
 # X_train, X_test, y_train, y_test = train_test_split(
 #     X, y, train_size=0.66, random_state=1)
@@ -53,11 +54,31 @@ stdsc = StandardScaler()
 
 X_std = stdsc.fit_transform(X)
 
-classifier = KNeighborsClassifier(
-    n_neighbors=1, metric='manhattan', algorithm='brute')
+classifier = RandomForestClassifier(
+    n_estimators=300, criterion='entropy', max_features='log2', n_jobs=-1)
 
-# classifier = RandomForestClassifier(
-#     n_estimators=300, criterion='entropy', max_features='log2', n_jobs=-1)
+# classifier = KNeighborsClassifier(
+#     n_neighbors=1, metric='manhattan', algorithm='brute')
+
+classifier.fit(X_std, y)
+model_file_path = os.path.join(
+    os.path.abspath(os.path.curdir),
+    'job', 'test.model')
+model_save = pickle.dump(classifier, open(model_file_path, 'wb'))
+
+test_file_path = os.path.join(
+    os.path.abspath(os.path.curdir),
+    'job', 'test.csv')
+test_data = pd.read_csv(test_file_path, skiprows=1, header=None)
+test_data_numeric = test_data.apply(pd.to_numeric, errors='coerce')
+X_test = test_data_numeric.values
+saved_model = pickle.load(open(model_file_path, 'rb'))
+pred = saved_model.predict(X_test)
+print(pred)
+# print('-------------')
+# print("Accuracy: %.2f%%" % (accuracy*100.0))
+# print('-------------')
+
 
 # classifier.fit(X_train_std, y_train)
 # model = classifier.predict(X_test_std)
@@ -66,5 +87,19 @@ classifier = KNeighborsClassifier(
 # kappa = metrics.cohen_kappa_score(y_test, model)
 
 kfold = KFold(n_splits=10, random_state=100, shuffle=True)
-results_kfold = cross_val_score(classifier, X_std, y, cv=kfold)
-print("Accuracy: %.2f%%" % (results_kfold.mean()*100.0))
+# cross_val = cross_val_score(classifier, X_std, y, cv=kfold)
+cross_val = cross_val_predict(classifier, X_std, y, cv=kfold)
+accuracy = metrics.accuracy_score(y, cross_val)
+f1_score = metrics.f1_score(y, cross_val, average='weighted')
+kappa = metrics.cohen_kappa_score(y, cross_val)
+print("Accuracy: %.2f%%" % (accuracy*100.0))
+print("F1-Score: %.2f%%" % (f1_score*100.0))
+print("Kappa: %.2f%%" % (kappa*100.0))
+print('-------------')
+conf_mat = metrics.confusion_matrix(y, cross_val)
+conf_mat_array = pd.DataFrame(conf_mat).to_csv(os.path.join(
+    os.path.abspath(os.path.curdir),
+    'job', 'output.csv'), header=None, index=None)
+
+# print(results_kfold)
+# print("Accuracy: %.2f%%" % (results_kfold.mean()*100.0))
